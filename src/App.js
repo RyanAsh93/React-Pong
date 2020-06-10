347e04029b8ed9d80f1d3c9e7b4f1003928945cd
RyanAsh93 <blueheadryan93@yahoo.com>
2020-06-10 16:18:41 -0600

    parshal setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Container } from 'semantic-ui-react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  state = {
+    squareX: 0,
+    squareY: 10,
+    dx: 5,
+    dy: 4,
+  };
+  componentDidMount() {
+    this.ticker = setInterval(() => {
+      this.tick();
+    }, 50);
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.squareX > length - squareSize) {
+      this.setState({
+        dx: -5,
+        squareX: length - squareSize,
+      });
+    }
+    if (prevState.squareX < 1) {
+      console.log(prevState.squareX);
+      this.setState({
+        dx: 5,
+        squareX: 1,
+      });
+    }
+    if (prevState.squareY > length - squareSize) {
+      this.setState({
+        dy: -4,
+        squareY: length - squareSize,
+      });
+    }
+    if (prevState.squareY < 1) {
+      this.setState({
+        dy: 4,
+        squareY: 1,
+      });
+    }
+  }
+  tick() {
+    const { squareX, squareY, dx, dy } = this.state;
+    const newSquareX = squareX + dx;
+    const newSquareY = squareY + dy;
+    this.setState({
+      squareX: newSquareX,
+      squareY: newSquareY,
+    });
+  }
+  render() {
+    const { squareX, squareY } = this.state;
+    return (
+      <div style={styles}>
+        <div style={{ ...squareStyles, top: squareY, left: squareX,}}></div>
+      </div>
+    );
+  }
 }
+const length = 600;
+const squareSize = 30;
 
-export default App;
+const squareStyles = {
+  position: "absolute",
+  background: "white",
+  width: `${squareSize}px`,
+  height: `${squareSize}px`,
+};
+const styles = {
+  position: "relative",
+  background: "black",
+  margin: "auto",
+  border: "1px solid",
+  width: `${length}px`,
+  height: `${length}px`,
+};
